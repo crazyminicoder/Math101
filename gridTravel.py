e10d8303b4d8b31d76ded75af617597e496ca3ce
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2024-10-16 12:40:41 -0500

    added check for key in memo

--- gridTravel.py
+++ gridTravel.py
@@ -1,15 +1,21 @@
 class Solution:
     def travel(self, m, n, memo):
-        key = str(m)+','+str(n)
+        # checking if the key is in memo if yes return it makes the program much faster
+        key = f"{m},{n}"
+        if key in memo:
+            return memo[key]
+
+        # base case
         if m == 1 and n == 1:
             return 1
         if m == 0 or n == 0:
             return 0
+
         memo[key] = self.travel(m, n-1, memo) + self.travel(m-1, n, memo)
         return memo[key]
 
 
 res = Solution()
 memo = {}
-a = res.travel(7, 7, memo)
+a = res.travel(29, 29, memo)
 print(a)
