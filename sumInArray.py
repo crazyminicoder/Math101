28b4d6043f872c2edf308947e37452ad64900fdf
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2024-10-18 12:23:28 -0500

    Added comments

--- sumInArray.py
+++ sumInArray.py
@@ -1,3 +1,8 @@
+# Given an array of non negative numbers return is there a war to acheiving the sum
+# eample Array[2,7,5,4] Target = 12
+# There are many ways of getting the sum
+# eg: 2+2+2+2+2+2+2+2+2+2+2+2= 12 or 7+5 = 12 given this return True if it is possible or else False
+
 class Solution:
     def isSum(self, array, target):
         if target == 0:
