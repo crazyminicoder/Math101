f5776c14078d39328f8b4828e97ef77566ace0ce
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2024-10-18 14:36:00 -0500

    Added function that returns the elements

--- sumInArray.py
+++ sumInArray.py
@@ -17,7 +17,23 @@ class Solution:
 
         return False
 
+    def howSum(self, array, target):
+        if target == 0:
+            return []
+        if target < 0:
+            return None
+
+        for num in array:
+            res = target-num
+            result = self.howSum(array, res)
+            if result is not None:
+                return result + [num]
+
+        return None
+
 
 res = Solution()
 a = res.isSum([2, 7, 5, 4], 56)
 print(a)
+b = res.howSum([7, 5, 2, 4], 12)
+print(b)
