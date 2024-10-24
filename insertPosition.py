669280522d9929bbbe822c13167a113768d93588
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2024-10-24 12:39:49 -0500

    Initial commit

---
+++ insertPosition.py
@@ -0,0 +1,11 @@
+class Solution:
+    def searchIndex(self, nums, target):
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+        return len(nums)
+
+
+s = Solution()
+res = s.searchIndex([-1, 3, 5, 6], -2)
+print(res)
