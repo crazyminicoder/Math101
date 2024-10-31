01954dcff42ce51781654d99bbba0fb531ed012c
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2024-10-31 09:33:47 -0500

    initial collot

---
+++ Anagram.py
@@ -0,0 +1,36 @@
+class Solution:
+    def anagram(self, s, t):
+        s1 = list(s)
+        d1 = {}
+        d2 = {}
+        if len(s) != len(t):
+            return False
+        for letter in s:
+            if letter in d1:
+                d1[letter] += 1
+            else:
+                d1[letter] = 1
+
+        for letter in t:
+            if letter in d2:
+                d2[letter] += 1
+            else:
+                d2[letter] = 1
+        for i in t:
+            if i in s1:
+                countInT = d2[i]
+                countInS = d1[i]
+                if countInT != countInS:
+                    return False
+            else:
+                return False
+
+        return True
+        # for i in t1:
+        # if i not in s1:
+        # return False
+
+
+s = Solution()
+res = s.anagram('rat', 'car')
+print(res)
