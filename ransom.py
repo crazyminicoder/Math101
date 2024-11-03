0d985d9e26262dba0136e773bb1793de486353ec
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2024-11-03 13:15:56 -0600

    initial commit

---
+++ ransom.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canConstruct(self, ransomNote, magazine):
+        ransomDict = {}
+        magazineDict = {}
+        rdSet = set()
+        mdSet = set()
+        for i in ransomNote:
+            rdSet.add(i)
+            if i in ransomDict:
+                ransomDict[i] += 1
+            else:
+                ransomDict[i] = 1
+
+        for i in magazine:
+            mdSet.add(i)
+            if i in magazineDict:
+                magazineDict[i] += 1
+            else:
+                magazineDict[i] = 1
+
+        for i in rdSet:
+            if ransomDict[i] != magazineDict[i] and ransomDict[i] > magazineDict[i]:
+                return False
+
+        return True
+
+
+s = Solution()
+a = s.canConstruct('ba', 'abb')
+print(a)
