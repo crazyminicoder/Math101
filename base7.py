5a75c6eb9236c3f5335c6b3f45b880074edd86d2
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2024-11-08 11:34:22 -0600

    initial commit

---
+++ base7.py
@@ -0,0 +1,14 @@
+class Solution:
+    def base7(self, num):
+        res = 0
+        while num > 0:
+            res *= 10
+            res += num % 7
+            num //= 7
+
+        return res
+
+
+s = Solution()
+resu = s.base7(100)
+print(resu)
