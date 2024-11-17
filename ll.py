d7a437f3e2ca6d065f0fd9bd9f2a734fdda751a6
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2024-11-16 21:37:41 -0600

    initial commit

---
+++ ll.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, data):
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+
+        temp.next = newNode
+
+    def printLL(self):
+        if self.head is None:
+            print('LinkedList is empty')
+            return
+        temp = self.head
+        while temp:
+            print(temp.data, end='->' if temp.next else '')
+            temp = temp.next
+
+
+ll = LinkedList()
+ll.add(5)
+ll.add(10)
+ll.add(15)
+ll.add(20)
+ll.add(25)
+ll.add(30)
+ll.add(35)
+
+ll.printLL()
