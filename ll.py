6692b28be6d3535a568296ca4b7eae6da814fd12
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2024-11-16 21:46:34 -0600

    update

--- ll.py
+++ ll.py
@@ -28,6 +28,25 @@ class LinkedList:
             print(temp.data, end='->' if temp.next else '')
             temp = temp.next
 
+    def remove(self, key):
+        if self.head is None:
+            print('LinkedList is empty')
+            return
+        if self.head.data == key:
+            self.head = self.head.next
+        else:
+            temp = self.head
+            prev = None
+            while temp:
+                if temp.data == key:
+                    if prev:
+                        prev.next = temp.next
+                    return
+                prev = temp
+                temp = temp.next
+
+            print('key not found')
+
 
 ll = LinkedList()
 ll.add(5)
@@ -37,5 +56,10 @@ ll.add(20)
 ll.add(25)
 ll.add(30)
 ll.add(35)
+
+ll.printLL()
+
+ll.remove(15)
+print()
 
 ll.printLL()
